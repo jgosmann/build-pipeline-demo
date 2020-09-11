52464c0648ff9392adfa19d3c30b739900bcac66
Lars Bonnes <lars.bonnes@tngtech.com>
2020-09-11 14:03:06 +0200

    Sample gradle project

---
+++ src/main/java/com/userlarsb/demo/BuildPiplineDemonstratorApplication.java
@@ -0,0 +1,13 @@
+package com.userlarsb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildPiplineDemonstratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildPiplineDemonstratorApplication.class, args);
+	}
+
+}
